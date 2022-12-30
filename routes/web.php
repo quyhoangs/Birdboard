d9c89199a585aded6bd21be5c9da16b8286d6de1
HoangddMH <Morhn@2022>
2022-12-30 17:30:50 +0700

    Update UI login and register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register',[RegisterController::class,'create'])->name('register');
-Route::post('/register',[RegisterController::class,'store'])->name('register');
+Route::post('/register',[RegisterController::class,'store']);
+Route::get('/login',[LoginController::class,'index'])->name('login');
 
 Route::controller(ProjectsController::class)->middleware(['auth'])->group(function () {
     Route::prefix('projects')->group(function() {
